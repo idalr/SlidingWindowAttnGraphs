91bb714c97c440f787fbe8d1eb70b70dbdc5979b
Ruangrin L <88072261+idalr@users.noreply.github.com>
2025-03-03 11:14:37 +0100

    minor fix

--- base_model.py
+++ base_model.py
@@ -201,12 +201,12 @@ class MHAClassifier(Classifier_Lighting):
             param.requires_grad = False
             
         #print ("Attention method:", activation_attention)
-        # full MHA
+        # sliding window MHA
         if window:
             self.attention = SlidingWindowMultiHeadSelfAttention(self.sent_model.get_sentence_embedding_dimension(),
                                                                  embed_dim, num_heads, window_size=30, temperature=1,
                                                                  dropout=attn_dropout, activation_attention=activation_attention)
-        # sliding window MHA
+        # full MHA
         else:
             self.attention = MultiHeadSelfAttention(self.sent_model.get_sentence_embedding_dimension(), embed_dim,
                                                     num_heads, temperature=1, dropout=attn_dropout,
